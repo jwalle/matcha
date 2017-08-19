8bc2b753ef7b85cf4353a373b953cf3b5cf97478
jwalle <jwalle@student.42.fr>
2017-08-19 19:00:59 +0200

    start

--- src/public/index.php
+++ src/public/index.php
@@ -4,11 +4,16 @@ use \Psr\Http\Message\ResponseInterface as Response;
 
 require '../vendor/autoload.php';
 
+$con = mysqli_connect("mysql", "jwalle", "root", "db_matcha") or die(mysqli_error());
+if (mysqli_connect_errno()) {
+	echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+echo "Connected to MySQL<br />";
+
 $app = new \Slim\App;
 $app->get('/hello/{name}', function (Request $request, Response $response) {
     $name = $request->getAttribute('name');
     $response->getBody()->write("Hello, $name");
-
     return $response;
 });
 $app->run();
